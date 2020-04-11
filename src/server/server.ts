b3bfd73657d193ec548d2cb52e8c06a77dc94d27
romain <romain.pillot@epitech.eu>
2020-04-11 23:26:06 +0100

    handles overflow

--- src/server/server.ts
+++ src/server/server.ts
@@ -10,27 +10,78 @@ const database = new Database(true);
 
 api.use(express.json());
 
-api.get('/api/articles', (req, res) => res.status(200).json(database.loadAll()));
+api.get('/', (req, res) => res.status(200).send('Welcome to the assignment03 API'));
 
-api.get('/api/article/:id', (req, res) => res.status(200).json(database.load(req.params.id)));
+api.get('/articles', (req, res) => res.status(200).json(database.loadAll()));
 
-api.post('/api/article', (req, res) => res.status(200).json(database.save(req.body as Article)));
+api.get('/article/:id', (req, res) => res.status(200).json(database.load(req.params.id)));
 
-api.put('/api/article', (req, res) => {
+api.post('/article', (req, res) => res.status(200).json(database.save(req.body as Article)));
+
+api.put('/article', (req, res) => {
   database.save(req.body as Article);
   res.status(200).end();
 });
 
-api.delete('/api/article/:id', (req, res) => {
+api.delete('/article/:id', (req, res) => {
   database.delete(req.params.id);
   res.status(200).end();
 });
 
+api.listen(API_PORT, 'localhost', () => console.log('API started with success'));
+
 ////////////////////////////////////////////////// SERVE FRONTEND //////////////////////////////////////////////////////////////////////////
+/**const app = express();
 
-api.use('/', express.static(`${__dirname}/../../dist/assignment03`));
+app.use('/', express.static(`${__dirname}/../../dist/assignment03`));
 
-api.listen(process.env.PORT || 80, () => {
+const request = require('request');
+app.use('/api', (req, res) => req.pipe(request(`http://localhost:${API_PORT}${req.url}`)).pipe(res));
+
+app.listen(HTTP_PORT, 'localhost', () => {
   console.log('Open your broswer, you should see the website at http://localhost');
   console.log('API is accessible using http://localhost/api');
 });
+**/
+
+/**
+ import {Database} from './database';
+ import {Article} from './article';
+
+ const API_PORT = 8080;
+ const HTTP_PORT = 80;
+ const express = require('express');
+ ////////////////////////////////////////////////// SERVE API ///////////////////////////////////////////////////////////////////////////////
+ const api = express();
+ const database = new Database(true);
+
+ api.use(express.json());
+
+ api.get('/api/articles', (req, res) => res.status(200).json(database.loadAll()));
+
+ api.get('/api/article/:id', (req, res) => res.status(200).json(database.load(req.params.id)));
+
+ api.post('/api/article', (req, res) => res.status(200).json(database.save(req.body as Article)));
+
+ api.put('/api/article', (req, res) => {
+  database.save(req.body as Article);
+  res.status(200).end();
+});
+
+ api.delete('/api/article/:id', (req, res) => {
+  database.delete(req.params.id);
+  res.status(200).end();
+});
+
+
+ ////////////////////////////////////////////////// SERVE FRONTEND //////////////////////////////////////////////////////////////////////////
+
+ api.use('/', express.static(`${__dirname}/../../dist/assignment03`));
+
+ api.listen(process.env.PORT || 80, () => {
+  console.log('Open your broswer, you should see the website at http://localhost');
+  console.log('API is accessible using http://localhost/api');
+});
+
+
+ */
